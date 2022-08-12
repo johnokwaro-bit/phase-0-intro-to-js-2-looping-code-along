3c0956e132fd3a2a9aa043d6cdf128813ad3e93b
John Okwaro <john.okwaro@moringaschool.com>
2022-08-12 16:05:19 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solutions in this file
+
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+
+function writeCards(array, event){
+
+    const newArray = [];
+
+    for(let i = 0; i < array.length; i++){
+        debugger
+        newArray[i]= `Thank you, ${array[i]}, for the wonderful ${event} gift!`;
+        
+    }
+    
+        
+    return newArray;
+}
+
+let i = ""
+
+function countDown(i){
+    while( i >= 0)
+
+    {
+        console.log(i);
+        i--;
+    }
+}
+
+countDown();
